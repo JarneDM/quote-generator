0132f0a92873fba45722685fc8d84d1d886caf2f
Jarne <jarne.demeyer05@hotmail.com>
2023-06-22 23:39:21 +0200

    adding quotes and style. + button and js

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,61 @@
 <script>
-	export let name;
+	export let quotes;
+	let random;
+	const randomQuote = ()=> {
+		random = Math.floor(Math.random() * quotes.length);
+	}
+
+	if (random == undefined){
+		randomQuote();
+	}
+
+	if (random > 0 && quotes[random] === quotes[random - 1]) {
+  		randomQuote();
+	}
+
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Random quote of the day:</h1>
+	<h2>{quotes[random]}</h2>
+	<button class="refresh" id="refresh" on:click={randomQuote}>Change</button>
 </main>
 
 <style>
 	main {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
 		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+		height: 100vh;
 	}
 
 	h1 {
-		color: #ff3e00;
+		color: #39d374;
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
+		margin-bottom: 1em;
+	}
+
+	h2 {
+		width: 100%;
+		max-width: 450px; 
+		color: white;
+	}
+
+	.refresh {
+		border-radius: 10px;
+		border: 2px solid #39d374;
+		height: 50px;
+		width: 100px;
+		margin-top: 5%;
+		cursor: pointer;
+	}
+
+	.refresh:hover{
+		background-color: lightgrey;
 	}
 
 	@media (min-width: 640px) {
--- src/main.js
+++ src/main.js
@@ -3,7 +3,24 @@ import App from './App.svelte';
 const app = new App({
 	target: document.body,
 	props: {
-		quotes: []
+		quotes: [
+			`“We cannot solve problems with the kind of thinking we employed when we came up with them.” — Albert Einstein`,
+		 	`“Learn as if you will live forever, live like you will die tomorrow.” — Mahatma Gandhi`,
+			`“When you change your thoughts, remember to also change your world.” — Norman Vincent Peale`,
+			`“If you are working on something that you really care about, you don’t have to be pushed. The vision pulls you.” — Steve Jobs`,
+			`“You learn more from failure than from success. Don’t let it stop you. Failure builds character.” — Unknown `,
+			`“I’m a greater believer in luck, and I find the harder I work the more I have of it.” — Thomas Jefferson`,
+			`“The strongest actions for a woman is to love herself, be herself and shine amongst those who never believed she could.” — Unknown`,
+			`“Try not to become a man of success, but rather become a man of value.” – Albert Einstein`,
+			`“Success is stumbling from failure to failure with no loss of enthusiasm.” ― Winston Churchill`,
+			`“Get a good idea and stay with it. Dog it, and work at it until it’s done right.” — Walt Disney`,
+			`“Work until your bank account looks like a phone number.” — Unknown`,
+			`“Coming together is a beginning. Keeping together is progress. Working together is success.” — Henry Ford`,
+			`“When you arise in the morning think of what a privilege it is to be alive, to think, to enjoy, to love…” – Marcus Aurelius`,
+			`“Be sure you put your feet in the right place, then stand firm.” ― Abraham Lincoln`,
+			`“Life is like riding a bicycle. To keep your balance you must keep moving.” — Albert Einstein`,
+			`"Why are you looking for quotes. Get to work and grind until you made it" — Me`
+		]
 	}
 });
 
